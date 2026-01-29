96c1c05584a718fd355e91bb770db7e79b094e47
edewen <edewen@gmail.com>
2026-01-29 14:12:54 -0500

    pct

--- WrathCombo/Combos/PvE/BLM/BLM_Config.cs
+++ WrathCombo/Combos/PvE/BLM/BLM_Config.cs
@@ -121,11 +121,9 @@ internal partial class BLM
 
                     DrawSliderInt(0, 100, BLM_ST_ThunderTrashOption,
                         "Non boss encounter. Stop using at Enemy HP %.");
-
-                    ImGui.Indent();
+                    
                     DrawSliderInt(0, 5, BLM_ST_ThunderRefresh,
                         "Seconds remaining before reapplying the DoT. Set to Zero to disable this check.");
-                    ImGui.Unindent();
                     break;
 
                 case Preset.BLM_ST_Manaward:
