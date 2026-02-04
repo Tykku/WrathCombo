3af793aae81e18b62a0dfbae0d1312755f19b753
edewen <edewen@gmail.com>
2026-02-03 22:31:12 -0500

    2 target dotting level checks

--- WrathCombo/Combos/PvE/AST/AST.cs
+++ WrathCombo/Combos/PvE/AST/AST.cs
@@ -91,7 +91,7 @@ internal partial class AST : Healer
                 ? SimpleTarget.DottableEnemy(dotAction, dotDebuffID, 0, 30, 99)
                 : SimpleTarget.DottableEnemy(dotAction, dotDebuffID, 0, 3, 2);
             
-            if (target is not null && CanApplyStatus(target, dotDebuffID) && !JustUsedOn(dotAction, target) && PartyInCombat())
+            if (target is not null && ActionReady(dotAction) && CanApplyStatus(target, dotDebuffID) && !JustUsedOn(dotAction, target) && PartyInCombat())
                 return dotAction.Retarget(replacedActions, target);
 
             return actionID;
@@ -336,7 +336,7 @@ internal partial class AST : Healer
                     return OriginalHook(Combust);
                 
                 //2 target Dotting System to maintain dots on 2 enemies. Works with the same sliders and one target
-                if (target is not null && CanApplyStatus(target, dotDebuffID) && !JustUsedOn(dotAction, target) && AST_ST_DPS_CombustUptime_TwoTarget)
+                if (target is not null && ActionReady(dotAction) && CanApplyStatus(target, dotDebuffID) && !JustUsedOn(dotAction, target) && AST_ST_DPS_CombustUptime_TwoTarget)
                     return dotAction.Retarget(replacedActions, target);
             }
 
