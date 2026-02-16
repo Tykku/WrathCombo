b2abb6c4319858d11d240be530453f94862a283b
Taurenkey <github@taurenkey.co.uk>
2026-02-16 18:18:48 +0000

    In Boss Fight Range Only

--- WrathCombo/AutoRotation/AutoRotationConfig.cs
+++ WrathCombo/AutoRotation/AutoRotationConfig.cs
@@ -30,6 +30,7 @@ public class DPSSettings
     public bool OnlyAttackInCombat = false;
     public bool DPSAlwaysHardTarget = false;
     public float MaxDistance = 25;
+    public bool IgnoreRangeInBoss = true;
     public bool AoEIgnoreManual = false;
     public bool UnTargetAndDisableForPenalty = false;
 }
