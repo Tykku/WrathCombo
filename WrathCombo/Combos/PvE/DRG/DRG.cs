92ff24b42de8efea56d5747a6d1d8869c455b74f
Kage <95875972+Kagekazu@users.noreply.github.com>
2026-03-18 14:14:54 +0100

    cleanup

--- WrathCombo/Combos/PvE/DRG/DRG.cs
+++ WrathCombo/Combos/PvE/DRG/DRG.cs
@@ -268,7 +268,7 @@ internal partial class DRG : Melee
                         //Lance Charge Feature
                         if (IsEnabled(Preset.DRG_ST_LanceCharge) &&
                             ActionReady(LanceCharge) &&
-                            (ActionReady(BattleLitany) || 
+                            (ActionReady(BattleLitany) ||
                              GetCooldownRemainingTime(BattleLitany) is > 50 and < 65 ||
                              !LevelChecked(BattleLitany)) &&
                             GetTargetHPPercent() > HPThresholdLanceCharge)
