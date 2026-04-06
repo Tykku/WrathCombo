aaddb09134a2f3cc9b1f9dd3b7f5f5dc8609840f
Kage <95875972+Kagekazu@users.noreply.github.com>
2026-04-06 23:53:17 +0200

    update MNK lvl 90 opener

--- WrathCombo/Combos/PvE/MNK/MNK_Helper.cs
+++ WrathCombo/Combos/PvE/MNK/MNK_Helper.cs
@@ -479,27 +479,21 @@ internal partial class MNK
         [
             ForbiddenMeditation,
             FormShift,
-            TwinSnakes,
-            Demolish,
-            TheForbiddenChakra,
             DragonKick,
-            Brotherhood,
             PerfectBalance,
             Bootshine,
-            RiddleOfWind,
+            DragonKick,
+            Bootshine,
             RiddleOfFire,
+            Brotherhood,
+            ElixirField,
+            DragonKick,
+            PerfectBalance,
+            Bootshine,
             DragonKick,
             Bootshine,
             ElixirField,
-            DragonKick,
-            TwinSnakes,
-            Demolish,
-            Bootshine,
-            PerfectBalance,
-            DragonKick,
-            Bootshine,
-            DragonKick,
-            ElixirField
+            DragonKick
         ];
 
         public override List<(int[] Steps, Func<bool> Condition)> SkipSteps { get; set; } =
@@ -532,27 +526,21 @@ internal partial class MNK
         [
             ForbiddenMeditation,
             FormShift,
-            TwinSnakes,
-            Demolish,
-            TheForbiddenChakra,
             DragonKick,
+            PerfectBalance,
+            Bootshine,
+            DragonKick,
+            Bootshine,
             Brotherhood,
-            PerfectBalance,
-            TwinSnakes,
-            RiddleOfWind,
             RiddleOfFire,
-            Demolish,
-            Bootshine,
-            RisingPhoenix,
+            ElixirField,
             DragonKick,
+            PerfectBalance,
+            Bootshine,
             TwinSnakes,
             Demolish,
-            Bootshine,
-            PerfectBalance,
-            DragonKick,
-            Bootshine,
-            DragonKick,
-            ElixirField
+            RisingPhoenix,
+            DragonKick
         ];
 
         public override List<(int[] Steps, Func<bool> Condition)> SkipSteps { get; set; } =
