d66359221749736baa796c346802aed834b1e726
Taurenkey <github@taurenkey.co.uk>
2026-05-01 09:08:24 +0100

    Add motion tracker to pausing statuses

--- WrathCombo/Data/StatusCache.cs
+++ WrathCombo/Data/StatusCache.cs
@@ -169,7 +169,8 @@ internal class StatusCache
             }.ToFrozenSet();
 
         internal static readonly FrozenSet<uint> Misc = new uint[] {
-            1735 // The Orbonne Monastary - Heavenly Shield
+            1735, // The Orbonne Monastary - Heavenly Shield
+            5191  // The Clyteum - Motion Tracker
         }.ToFrozenSet();
 
     }
