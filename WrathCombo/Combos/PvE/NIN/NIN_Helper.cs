8f3924ee5e1046a31abe721f03fbb58cc9de90c7
Taurenkey <github@taurenkey.co.uk>
2026-07-24 17:43:29 +0100

    1.0.4.18

--- WrathCombo/Combos/PvE/NIN/NIN_Helper.cs
+++ WrathCombo/Combos/PvE/NIN/NIN_Helper.cs
@@ -17,7 +17,7 @@ internal partial class NIN
 {
     static NINGauge gauge = GetJobGauge<NINGauge>();
     public static FrozenSet<uint> MudraSigns = [Ten, Chi, Jin, TenCombo, ChiCombo, JinCombo];
-    public static FrozenSet<uint> NormalJutsus = [FumaShuriken, Raiton, Katon, Doton, Suiton, Hyoton, HyoshoRanryu, GokaMekkyaku, Rabbit];
+    public static FrozenSet<uint> NormalJutsus = [FumaShuriken, Raiton, Katon, Hyoton, Doton, Suiton, Huton, HyoshoRanryu, GokaMekkyaku, Rabbit];
     public static FrozenSet<uint> TCJJutsus = [TCJFumaShurikenChi, TCJFumaShurikenJin, TCJFumaShurikenTen, TCJRaiton, TCJKaton, TCJHyoton, TCJHuton, TCJSuiton, TCJDoton];
     internal static bool STSimpleMode => IsEnabled(Preset.NIN_ST_SimpleMode);
     internal static bool AoESimpleMode => IsEnabled(Preset.NIN_AoE_SimpleMode);
