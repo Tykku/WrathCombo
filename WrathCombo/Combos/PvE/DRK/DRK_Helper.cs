3b1c2bd2a9327da6d07d651c8866251445d105a1
Taurenkey <github@taurenkey.co.uk>
2026-09-06 08:31:09 +0100

    Update DRK

--- WrathCombo/Combos/PvE/DRK/DRK_Helper.cs
+++ WrathCombo/Combos/PvE/DRK/DRK_Helper.cs
@@ -241,80 +241,65 @@ internal partial class DRK
     internal static DRKOpenerMaxLevel1 Opener1 = new();
     internal static DRKOpenerEarlyBuff OpenerEarlyBuff = new();
 
-    internal class DRKOpenerMaxLevel1 : WrathOpener
+    internal abstract class DRKOpenerBase : WrathOpener
     {
         public override int MinOpenerLevel => 100;
-
         public override int MaxOpenerLevel => 109;
 
+        public static uint Pull => DRK_ST_OpenerAction.Value switch
+        {
+            1 => Shadowstride,
+            2 => HardSlash,
+            _ => Unmend
+        };
+    }
+
+    internal class DRKOpenerMaxLevel1 : DRKOpenerBase
+    {
         public override List<Func<uint>> OpenerActions { get; set; } =
         [
-            () => Unmend, // 1
-            () => Items.UseItem(Items.GetStrongestPotionRow(Items.PotionType.Strength)), // 2
-            () => HardSlash, // 3
-            () => EdgeOfShadow, // 4
-            () => LivingShadow, // 5
-            () => SyphonStrike, // 6
-            () => LivingShadow, // 7
-            () => Souleater, // 8
-            () => Delirium, // 9
-            () => HardSlash, // 10
-            () => Disesteem, // 11
-            () => SaltedEarth, // 12
-            () => ScarletDelirium, // 13
-            () => Shadowbringer, // 14
-            () => Comeuppance, // 15
-            () => CarveAndSpit, // 16
-            () => Torcleaver, // 17
-            () => Shadowbringer, // 18
-            () => Bloodspiller, // 19
-            () => SaltAndDarkness, // 20
+            () => All.Cease, // 1
+            () => Pull, // 2
+            () => Items.UseItem(Items.GetStrongestPotionRow(Items.PotionType.Strength)), // 3
+            () => HardSlash, // 4
+            () => EdgeOfShadow, // 5
+            () => LivingShadow, // 6
+            () => SyphonStrike, // 7
+            () => LivingShadow, // 8
+            () => Souleater, // 9
+            () => Delirium, // 10
+            () => HardSlash, // 11
+            () => Disesteem, // 12
+            () => SaltedEarth, // 13
+            () => ScarletDelirium, // 14
+            () => Shadowbringer, // 15
+            () => Comeuppance, // 16
+            () => CarveAndSpit, // 17
+            () => Torcleaver, // 18
+            () => Shadowbringer, // 19
+            () => Bloodspiller, // 20
+            () => SaltAndDarkness, // 21
         ];
 
-        public override List<(int[] Steps, uint NewAction, Func<bool> Condition)> SubstitutionSteps
-        {
-            get;
-            set;
-        } =
-        [
-            // Pull with Shadowstride as selected
-            ([1], Shadowstride, () =>
-                DRK_ST_OpenerAction == (int)PullAction.Shadowstride),
-            // Pull with HardSlash as selected (requires skipping the now-duplicate HardSlash)
-            ([1], HardSlash, () =>
-                DRK_ST_OpenerAction == (int)PullAction.HardSlash),
+        public override List<(int[] Steps, Func<float> HoldDelay)> PrepullDelays { get; set; } =
+[
+            ([2], () => Math.Max(0, CountdownRemaining - (Pull is Unmend ? 1 : Pull is Shadowstride ? 0.7f : 0))),
         ];
 
-        public override List<(int[] Steps, Func<bool> Condition)> SkipSteps
-        {
-            get;
-            set;
-        } =
+        public override List<(int[] Steps, Func<bool> Condition)> SkipSteps { get; set; } =
         [
-            // Skip the duplicate HardSlash, if pulling with HardSlash
-            ([3], () =>
-                DRK_ST_OpenerAction == (int)PullAction.HardSlash),
-            // Skip the early LivingShadow, if non-standard
-            ([5], () =>
-                DRK_ST_OpenerAction != (int)PullAction.Unmend),
-            // Skip the late LivingShadow and aligning HardSlash, if Standard
-            ([7, 10], () => Bursting.PartyIsBursting ||
-                DRK_ST_OpenerAction == (int)PullAction.Unmend),
-            // Skip Salted Earth
-            ([12], () =>
-                IsOnCooldown(SaltedEarth)),
-            // Skip Blood spenders when no Blood
-            ([19], () =>
-                Gauge.Blood < 50),
-            // Skip Salt and Darkness
-            ([20], () =>
-                !ActionReady(SaltAndDarkness)),
+            ([1], () => CountdownActive || InCombat() || !DRK_Opener_PrepullBlock),
+            ([4], () => DRK_ST_OpenerAction == (int)PullAction.HardSlash), // Skip the duplicate HardSlash, if pulling with HardSlash
+            ([6], () => DRK_ST_OpenerAction != (int)PullAction.Unmend), // Skip the early LivingShadow, if non-standard
+            ([8, 11], () => Bursting.PartyIsBursting || DRK_ST_OpenerAction == (int)PullAction.Unmend), // Skip the late LivingShadow and aligning HardSlash, if Standard
+            ([13], () => IsOnCooldown(SaltedEarth)), // Skip Salted Earth
+            ([20], () => Gauge.Blood < 50), // Skip Blood spenders when no Blood
+            ([21], () => !ActionReady(SaltAndDarkness)), // Skip Salt and Darkness
         ];
 
         public override Preset Preset => Preset.DRK_ST_BalanceOpener;
 
-        internal override UserData? ContentCheckConfig =>
-            DRK_ST_OpenerDifficulty;
+        internal override UserData? ContentCheckConfig => DRK_ST_OpenerDifficulty;
 
         internal override bool IncludePot => DRK_Opener_Potion;
 
@@ -326,92 +311,55 @@ internal partial class DRK
             (!InCombat() || CombatEngageDuration().TotalSeconds < 3);
     }
 
-    internal class DRKOpenerEarlyBuff : WrathOpener
+    internal class DRKOpenerEarlyBuff : DRKOpenerBase
     {
-        public override int MinOpenerLevel => 100;
-
-        public override int MaxOpenerLevel => 109;
-
         public override List<Func<uint>> OpenerActions { get; set; } =
         [
             () => All.Cease, // 1
             () => LivingShadow, // 2
-            () => Unmend, // 3
+            () => Pull, // 3
             () => Items.UseItem(Items.GetStrongestPotionRow(Items.PotionType.Strength)), // 4
             () => EdgeOfShadow, // Not handled like a procc, since it sets up Darkside | 5
             () => HardSlash, // 6
             () => Delirium, // 7
             () => SaltedEarth, // 8
-            () => HardSlash, // 9
-            () => Disesteem, // 10
-            () => CarveAndSpit, // 11
-            () => ScarletDelirium, // 12
-            () => Shadowbringer, // 13
-            () => Comeuppance, // 14
-            () => Shadowbringer, // 15
-            () => Torcleaver, // 16
-            () => SaltAndDarkness, // 17
-            () => SyphonStrike, // 18
-            () => Souleater, // 19
-            () => Bloodspiller, // 20
-            () => HardSlash, // 21
+            () => Disesteem, // 9
+            () => CarveAndSpit, // 10
+            () => ScarletDelirium, // 11
+            () => Shadowbringer, // 12
+            () => Comeuppance, // 13
+            () => Shadowbringer, // 14
+            () => Torcleaver, // 15
+            () => SaltAndDarkness, // 16
+            () => SyphonStrike, // 17
+            () => Souleater, // 18
+            () => Bloodspiller, // 19
+            () => HardSlash, // 20
         ];
 
-        public override List<(int[] Steps, Func<float> HoldDelay)> PrepullDelays
-        {
-            get;
-            set;
-        } =
+        public override List<(int[] Steps, Func<float> HoldDelay)> PrepullDelays { get; set; } =
         [
             ([2], () => Math.Max(0, CountdownRemaining - 3)),
-            ([3], () => Math.Max(0, CountdownRemaining - 1)),
+            ([3], () => Math.Max(0, CountdownRemaining - (Pull is Unmend ? 1 : Pull is Shadowstride ? 0.7f : 0))),
         ];
 
-        public override List<(int[] Steps, uint NewAction, Func<bool> Condition)> SubstitutionSteps
-        {
-            get;
-            set;
-        } =
+        public override List<(int[] Steps, Func<bool> Condition)> SkipSteps { get; set; } =
         [
-            ([3], Shadowstride, () =>
-                DRK_ST_OpenerAction == (int)PullAction.Shadowstride),
-            ([3], HardSlash, () =>
-                DRK_ST_OpenerAction == (int)PullAction.HardSlash),
-        ];
-
-        public override List<(int[] Steps, Func<bool> Condition)> SkipSteps
-        {
-            get;
-            set;
-        } =
-        [
-            ([1], () => CountdownActive || InCombat()),
-            // Skip the duplicate HardSlash, if pulling with HardSlash
-            ([6], () =>
-                DRK_ST_OpenerAction == (int)PullAction.HardSlash),
-            // Skip Salted Earth if on cooldown
-            ([8], () =>
-                IsOnCooldown(SaltedEarth)),
-            // Skip the aligning HardSlash, if pulling with Unmend
-            ([9], () =>
-                DRK_ST_OpenerAction == (int)PullAction.Unmend),
-            // Skip Salt and Darkness when not ready
-            ([17], () =>
-                !ActionReady(SaltAndDarkness)),
-            // Skip Blood spenders when no Blood
-            ([20], () =>
-                Gauge.Blood < 50),
+            ([1], () => CountdownActive || InCombat() || !DRK_Opener_PrepullBlock),
+            ([6], () => DRK_ST_OpenerAction == (int)PullAction.HardSlash), // Skip the duplicate HardSlash, if pulling with HardSlash
+            ([8], () => IsOnCooldown(SaltedEarth)), // Skip Salted Earth if on cooldown
+            ([9], () => DRK_ST_OpenerAction == (int)PullAction.Unmend), // Skip the aligning HardSlash, if pulling with Unmend
+            ([16], () => !ActionReady(SaltAndDarkness)), // Skip Salt and Darkness when not ready
+            ([19], () => Gauge.Blood < 50), // Skip Blood spenders when no Blood
         ];
 
         public override Preset Preset => Preset.DRK_ST_BalanceOpener;
 
-        internal override UserData? ContentCheckConfig =>
-            DRK_ST_OpenerDifficulty;
+        internal override UserData? ContentCheckConfig => DRK_ST_OpenerDifficulty;
 
         internal override bool IncludePot => DRK_Opener_Potion;
 
         public override bool HasCooldowns() =>
-            CountdownActive &&
             LocalPlayer.CurrentMp > 7000 && IsOffCooldown(LivingShadow) &&
             IsOffCooldown(Delirium) && IsOffCooldown(CarveAndSpit) &&
             IsOffCooldown(SaltedEarth) &&
