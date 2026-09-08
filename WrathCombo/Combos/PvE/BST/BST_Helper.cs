6e9004eec6fcd1453a2b87e380565d2c83bfe3a9
Taurenkey <github@taurenkey.co.uk>
2026-09-08 09:55:49 +0100

    BST action IDs

--- WrathCombo/Combos/PvE/BST/BST_Helper.cs
+++ WrathCombo/Combos/PvE/BST/BST_Helper.cs
@@ -17,4 +17,26 @@ namespace WrathCombo.Combos.PvE;
 internal partial class BST
 {
 
+    public const uint
+        SmashAxe = 44879,
+        Capture = 44880,
+        FirstBattlehorn = 44881,
+        Gauge = 44882,
+        AxebladeBite = 44883,
+        AvalancheAxe = 44884,
+        Shieldsplitter = 44885,
+        BeastMode = 44886,
+        MistralAxe = 44887,
+        SpinningAxe = 44888,
+        GaleAxe = 44889,
+        TemperedRelease = 44890,
+        PartingBlow = 44891,
+        SecondBattlehorn = 44892,
+        ShieldCharge = 44893,
+        ThirdBattlehorn = 44894,
+        Borrow = 44895,
+        RallyingCheer = 44904,
+        Rally = 44905,
+        Trick = 47093;
+
 }
